e22aa02f8bb05bed6cec53156b5eedc884a7e73e
BentiGorlich <benjamin_g@posteo.de>
2024-04-05 20:01:42 +0200

    Adjust colors, remove card, fix repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,102 +4,100 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Mbin Docs',
-  tagline: '',
-  favicon: 'img/favicon.ico',
+    title: 'Mbin Docs',
+    tagline: '',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://docs.joinmbin.org',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://docs.joinmbin.org',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'mbinOrg', // Usually your GitHub org/user name.
-  projectName: 'mbin', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'mbinOrg', // Usually your GitHub org/user name.
+    projectName: 'mbin', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/MbinOrg/mbin/tree/main/docs',
-        },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: './sidebars.js',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: 'https://github.com/MbinOrg/mbin/tree/main/',
+                },
+                blog: false,
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Mbin Docs',
-        logo: {
-          alt: 'Mbin Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            href: 'https://github.com/mbinOrg/mbin',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-            {
-                title: 'Community',
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'Mbin Docs',
+                logo: {
+                    alt: 'Mbin Logo',
+                    src: 'img/logo.svg',
+                },
                 items: [
-                  {
-                    label: 'Mbin Dev Magazine',
-                    href: 'https://kbin.run/m/Mdev',
-                  },
-                  {
-                    label: 'Mbin Releases Magazine',
-                    href: 'https://gehirneimer.de/m/mbinReleases',
-                  },
-                  {
-                    label: 'Matrix Space',
-                    href: 'https://matrix.to/#/#mbin:melroy.org',
-                  }
+                    {
+                        href: 'https://github.com/mbinOrg/mbin',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
                 ],
-              },
-        ],
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Mbin Dev Magazine',
+                                href: 'https://kbin.run/m/Mdev',
+                            },
+                            {
+                                label: 'Mbin Releases Magazine',
+                                href: 'https://gehirneimer.de/m/mbinReleases',
+                            },
+                            {
+                                label: 'Matrix Space',
+                                href: 'https://matrix.to/#/#mbin:melroy.org',
+                            }
+                        ],
+                    },
+                ],
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
 };
 
 export default config;
