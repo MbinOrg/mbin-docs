abc5cfb8d2d1b8fb060099cdca3d64505de496ca
BentiGorlich <benjamin_g@posteo.de>
2024-04-07 20:02:16 +0200

    Only warn on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
     organizationName: 'mbinOrg', // Usually your GitHub org/user name.
     projectName: 'mbin', // Usually your repo name.
 
-    onBrokenLinks: 'throw',
+    onBrokenLinks: 'warn',
     onBrokenMarkdownLinks: 'warn',
 
     // Even if you don't use internationalization, you can use this field to set
