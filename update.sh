fc741d8b9f314e3e463629d9b56fbc5cdef3710a
BentiGorlich <benjamin_g@posteo.de>
2024-04-07 20:24:34 +0200

    Copy files to correct location

--- update.sh
+++ update.sh
@@ -45,8 +45,9 @@ if [[ $(git rev-list HEAD...origin/"$branch_name" --count) -gt 0 ]]; then
     # copy all .md files and folders from the source repo
     rsync --mkpath -f'+ */' -f'+ *.md' -f'- *' -r ./mbin-repo/docs ./
     cp ./mbin-repo/docs/images -r ./docs/
-    cp ./mbin-repo/CONTRIBUTING.md ./docs/contributing/README.md
-    cp ./mbin-repo/C4.md ./mbin-repo/LICENSE ./docs/contributing/
+    cp ./mbin-repo/CONTRIBUTING.md ./docs/04-contributing/README.md
+    cp ./mbin-repo/C4.md ./docs/04-contributing/
+    cp ./mbin-repo/LICENSE ./docs/04-contributing/LICENSE.md
     npm run build
 
     # Set post command
